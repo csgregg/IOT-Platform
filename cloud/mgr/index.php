c1721f75054bc11c145c786803fd760c27d8d878
csgregg <csgregg@users.noreply.github.com>
2022-11-14 20:30:21 +0000

    a

--- cloud/mgr/index.php
+++ cloud/mgr/index.php
@@ -27,22 +27,16 @@ if (!securePage($_SERVER['PHP_SELF'])) {
 <?php
 require("phpMQTT.php");
 require("mq-broker.php");
+$message = "Hello CloudAMQP MQTT!";
+//MQTT client id to use for the device. "" will generate a client id automatically
 $mqtt = new bluerhinos\phpMQTT($host, $port, "ClientID".rand());
 
-if(!$mqtt->connect(true,NULL,$username,$password)){
-  exit(1);
-}
-
-//currently subscribed topics
-$topics['topic'] = array("qos"=>0, "function"=>"procmsg");
-$mqtt->subscribe($topics,0);
-
-while($mqtt->proc()){
-}
-
-$mqtt->close();
-function procmsg($topic,$msg){
-  echo "Msg Recieved: $msg";
+if ($mqtt->connect(true,NULL,$username,$password)) {
+  $mqtt->publish("topic",$message, 0);
+  $mqtt->close();
+}else{
+  echo "Fail or time out
+";
 }
 
 ?>
