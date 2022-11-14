134e88357215fa24e3fae00c5dcc411986b0cc19
csgregg <csgregg@users.noreply.github.com>
2022-11-14 20:25:14 +0000

    mq

--- cloud/mgr/index.php
+++ cloud/mgr/index.php
@@ -24,6 +24,33 @@ if (!securePage($_SERVER['PHP_SELF'])) {
 }
 ?>
 
+<?php
+require("phpMQTT.php");
+require("mq-broker.php");
+$mqtt = new bluerhinos\phpMQTT($host, $port, "ClientID".rand());
+
+if(!$mqtt->connect(true,NULL,$username,$password)){
+  exit(1);
+}
+
+//currently subscribed topics
+$topics['topic'] = array("qos"=>0, "function"=>"procmsg");
+$mqtt->subscribe($topics,0);
+
+while($mqtt->proc()){
+}
+
+$mqtt->close();
+function procmsg($topic,$msg){
+  echo "Msg Recieved: $msg";
+}
+
+?>
+
+
+
+
+
 <div class="row">
 	<div class="col-sm-12">
 Hello Toby
