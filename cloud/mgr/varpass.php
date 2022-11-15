3a76a7adcc50ce44da467e3a62797f2e4b474f1e
Chris Gregg <chris@greggs.org>
2022-11-15 16:40:43 +0000

    asdf

--- cloud/mgr/varpass.php
+++ cloud/mgr/varpass.php
@@ -4,7 +4,7 @@
 <script>
     function reqListener (){}
 
-    function isJson(str) {
+    function isJSON(str) {
         try {
             JSON.parse(str);
         } catch (e) {
@@ -19,13 +19,17 @@
         // This is where you handle what to do with the response.
         // The actual data is found on this.responseText
 
-        var vars = JSON.parse(this.responseText);
+        if( isJSON(this.responseText) ) {
+            var vars = JSON.parse(this.responseText);
+        
+            // Split JSON and load into local variables
+            Object.keys(vars).forEach( function(key) { window[key] = vars[key]; } );
 
-        // Split JSON and load into local variables
-        Object.keys(vars).forEach( function(key) { window[key] = vars[key]; } );
-
-        console.log(host);
-        console.log(pwd);
+            console.log(host);
+            console.log(pwd);
+        } else {
+            console.log("Not logged in");
+        }
     }
 
     oReq.open("get", "vardata.php", true);
