4143c6e02a06a52c2087f7bc3538fcfaa23dda95
Chris Gregg <chris@greggs.org>
2022-11-15 20:50:51 +0000

    reorg

---
+++ cloud/core/varpasser.js
@@ -0,0 +1,38 @@
+// Variable passer
+
+function reqListener (){}
+
+function isJSON(str) {
+    try {
+        JSON.parse(str);
+    } catch (e) {
+        return false;
+    }
+    return true;
+}
+
+var oReq = new XMLHttpRequest(); // New request object
+
+oReq.onload = function() {
+    // This is where you handle what to do with the response.
+    // The actual data is found on this.responseText
+
+    if( isJSON(this.responseText) ) {
+        var vars = JSON.parse(this.responseText);
+    
+        // Split JSON and load into local variables
+        Object.keys(vars).forEach( function(key) {
+            window[key] = vars[key];
+            console.log( vars[key] );
+        } );
+
+    } else {
+        console.log("Not logged in");
+    }
+}
+
+oReq.open("get", "vardata.php", true);
+//                               ^ Don't block the rest of the execution.
+//                                 Don't wait until the request finishes to
+//                                 continue.
+oReq.send();
