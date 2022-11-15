41ead875e2ff04584a506c3a3edce37043bc616f
Chris Gregg <chris@greggs.org>
2022-11-15 16:20:26 +0000

    split

--- cloud/mgr/varpass.php
+++ cloud/mgr/varpass.php
@@ -10,7 +10,16 @@
     oReq.onload = function() {
         // This is where you handle what to do with the response.
         // The actual data is found on this.responseText
-        alert(this.responseText); // Will alert: 42
+
+        var vars = JSON.parse(this.responseText);
+
+        Object.keys(vars).foreach(function(key) {
+            console.log('Key : ' + key + ', Value : ' + data[key]);
+        }
+        )
+
+        alert(); // Will alert: 42
+        alert(this.responseTest[])
     };
     oReq.open("get", "vardata.php", true);
     //                               ^ Don't block the rest of the execution.
