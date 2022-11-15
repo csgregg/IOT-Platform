dea919fd4c3834b03fcdb0a6ac3cf96d173980d9
Chris Gregg <chris@greggs.org>
2022-11-15 16:52:59 +0000

    sdf

--- cloud/mgr/index.php
+++ cloud/mgr/index.php
@@ -22,7 +22,7 @@ require_once $abs_us_root.$us_url_root.'users/includes/template/prep.php';
 if (!securePage($_SERVER['PHP_SELF'])) {
     die();
 }
-require_once 'vardata.php';
+require_once 'varpass.php';
 
 ?>
 
